14d9f8a18a7d26ca29f3b93d3ee2085f8e8dc76e
joyining <yining1204@gmail.com>
2018-05-21 17:51:41 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+import Logo from './components/logo';
+
+ReactDOM.render(<Logo />, document.getElementById('root'));
