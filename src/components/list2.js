4d4f873b6b63125785ea6c7c60671f552e1996a8
johnny <nintendods@gmail.com>
2018-05-23 17:46:12 +0800

    add list-page

--- src/components/list2.js
+++ src/components/list2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../sass/page.scss';
+// import '../sass/page.scss';
+import '../sass/list.scss';
 import Titantic from '../images/1997_Titanic.jpg';
 import Holland from '../images/1996_Mr_Holland_Opus.jpg';
 import Terminator from '../images/1991_Terminator2.jpg';
