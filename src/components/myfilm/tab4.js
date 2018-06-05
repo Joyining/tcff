3946f7dcbc762f4ef97e77031fdf81270cddbb02
MikeWHC <mroptsking@gmail.com>
2018-06-05 23:44:11 +0800

    revise tab4

--- src/components/myfilm/tab4.js
+++ src/components/myfilm/tab4.js
@@ -7,8 +7,147 @@ import './tab4.scss';
 class Tab4 extends Component {
     constructor(props) {
         super(props);
+        this.datas = [];
+        this.state = {
+            films:[],
+            cffilms:[]
+        };
+    }
+    componentDidUpdate(){
+        let ar0 = this.state.cffilms.slice(0);
+        console.log("init",ar0)
+        console.log("state",this.state);
+        let cffilms = this.state.cffilms;
+        let ar1 = cffilms.reduce((a, x) => {
+            let l = a.length;
+            let lastObj = a[l-1];
+            let order_date = x.order_date.slice(0,x.order_date.indexOf(' '));
+            delete x.order_date;
+            delete x.id_member;
+            delete x.id;
+            if (lastObj === undefined) {
+                let obj = {
+                    order_date: order_date,
+                    orders: [x]
+                };
+                a.push(obj);
+            }else if (order_date === lastObj.order_date) {
+                // let ar = lastObj.orders;
+                // ar.push(x);
+                lastObj.orders.push(x);
+            }else if(order_date !== lastObj.order_date){
+                let obj = {
+                    order_date: order_date,
+                    orders: [x]
+                };
+                a.push(obj);
+            }
+            return a;
+        },[]);
+        cffilms = ar1;
+        console.log("first: ", ar1);
+        let ar2 = cffilms.map(order => {
+            let orders = order.orders.reduce((a,x) => {
+                let l = a.length;
+                let lastObj = a[l - 1];
+                // let order_date = x.order_date.slice(0, x.order_date.indexOf(' '));
+                // let id_movie = x.id_movie;
+                // delete x.order_date;
+                // delete x.id_member;
+                // delete x.id;
+                if (lastObj === undefined) {
+                    let obj = {
+                        id_movie: x.id_movie,
+                        quantity: x.quantity
+                    };
+                    a.push(obj);
+                } else if (x.id_movie === lastObj.id_movie) {
+                    // let ar = lastObj.orders;
+                    // ar.push(x);
+                    lastObj.quantity += x.quantity;
+                } else if (x.id_movie !== lastObj.id_movie) {
+                    let obj = {
+                        id_movie: x.id_movie,
+                        quantity: x.quantity
+                    };
+                    a.push(obj);
+                }
+                return a;
+            },[]);
+            order.orders = orders;
+            return order;
+        })
+        cffilms = ar2;
+        console.log("second: ", ar2);
+
+
+        let films = this.state.films;
+        let arr1 = films.reduce((a, x) => {
+            let l = a.length;
+            let lastObj = a[l - 1];
+            let order_date = x.order_date.slice(0, x.order_date.indexOf(' '));
+            delete x.order_date;
+            delete x.id_member;
+            delete x.id;
+            if (lastObj === undefined || order_date !== lastObj.order_date) {
+                let obj = {
+                    order_date: order_date,
+                    orders: [x]
+                };
+                a.push(obj);
+            } else if (order_date === lastObj.order_date) {
+                // let ar = lastObj.orders;
+                // ar.push(x);
+                lastObj.orders.push(x);
+            }
+            return a;
+        }, []);
+        films = arr1;
+        console.log("first: ", arr1);
+        let arr2 = films.map(order => {
+            let orders = order.orders.reduce((a, x) => {
+                let l = a.length;
+                let lastObj = a[l - 1];
+                if (lastObj === undefined || x.id_session !== lastObj.id_session) {
+                    let obj = {
+                        id_session: x.id_session,
+                        quantity: x.quantity,
+                        seats: [x.seat]
+                    };
+                    a.push(obj);
+                } else if (x.id_session === lastObj.id_session) {
+                    // let ar = lastObj.orders;
+                    // ar.push(x);
+                    lastObj.quantity += x.quantity;
+                    lastObj.seats.push(x.seat);
+                }
+                return a;
+            }, []);
+            order.orders = orders;
+            return order;
+        })
+        films = arr2;
+        console.log("second: ", arr2);
+        console.log("final state: ", this.state);
     }
     componentDidMount() {
+        let data = [];
+        fetch(`${process.env.PUBLIC_URL}/json/orders_raw.json`)
+        .then(res => res.json())
+        .then(datas => {
+            console.log("datas",datas.length);
+            datas = datas.filter((d) => {
+                return d.id_member == 15;
+            })
+            let cffilms = datas.filter((d) => {
+                return d.id_session == 0;
+            })
+            let films = datas.filter((d) => {
+                return d.id_movie == 0;
+            })
+            console.log("datas", datas.length);
+            this.setState({films:films,cffilms:cffilms});
+        });
     }
     render() {
         return (
