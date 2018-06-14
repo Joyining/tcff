c6f60735b072bb713b8143535e8b99a020e1b493
yen <good.a-666@yahoo.com.tw>
2018-06-14 19:03:58 +0800

    0614 07:03

--- src/components/membercenter.js
+++ src/components/membercenter.js
@@ -8,12 +8,14 @@ import memberCenterPic from "../images/member_center.svg";
 class MemberCenter extends Component{
    
     componentDidMount(){
-        $('#oders_click').click(function(){
-            if($(this).siblings('li').hasClass('on')){
-                $(this).siblings("li").slideUp(500).removeClass("on");  
+        $('.oders_post_down').hide();
+
+        $('.oders_post_box ul').click(function(){
+            if($(this).siblings('.oders_post_down').hasClass('on')){
+                $(this).siblings(".oders_post_down").slideUp(500).removeClass("on");  
             } else{
-                $(this).parent().siblings().children("li").removeClass("on");
-                $(this).siblings("li").slideDown(500).addClass("on").parent().siblings().children("li").slideUp(500);
+                $(this).parent().siblings('.oders_post_down').removeClass("on");
+                $(this).siblings(".oders_post_down").slideDown(500).addClass("on").parent().siblings().children("li").slideUp(500);
             }
         })
 
@@ -33,7 +35,8 @@ class MemberCenter extends Component{
                         <div className="photo">
                             <figure className="pic">
                                 <img src={memberCenterPic}/>
-                            </figure>
+                                <button className="pic_change">更換照片</button>
+                            </figure>                            
                             <div className="btns">
                                 <div className="box_click">訂單查詢</div>
                                 <div className="box_unclick">修改會員資料</div>
@@ -48,13 +51,14 @@ class MemberCenter extends Component{
                                <div className="oders month_word">訂單查詢</div>
                                <div className="oders_use">
                                    <ul  className="oders_post_box">
-                                   
-                                       <ul className="oders_post oders_word" id="oders_click">22</ul>
-                                            <li className="oders_post_down oders_word">155</li>
-                    
-                                       <ul className="oders_post oders_word" id="oders_click">22</ul>
-                                            <li className="oders_post_down oders_word">155</li>
-                                  
+                                        <li>
+                                            <ul className="oders_post oders_word" id="oders_click">06/21</ul>
+                                            <li className="oders_post_down oders_word">訂單內容</li>
+                                        </li>
+                                        <li>
+                                            <ul className="oders_post oders_word" id="oders_click">06/18</ul>
+                                            <li className="oders_post_down oders_word">訂單內容</li>
+                                        </li>
                                    </ul>
                                   
 
